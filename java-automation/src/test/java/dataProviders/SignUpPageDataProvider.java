d1e53d75651044a244e0cf9219d01dd31c48f6fd
ionutthecreator <ionut.hodoroaga20@gmail.com>
2023-05-11 21:32:45 +0300

    signup page setup, data provider example

---
+++ java-automation/src/test/java/dataProviders/SignUpPageDataProvider.java
@@ -0,0 +1,17 @@
+package dataProviders;
+
+import org.testng.annotations.DataProvider;
+
+public class SignUpPageDataProvider {
+
+    @DataProvider (name = "signUpPageDataProvider")
+    public Object [][] SignUpPageDataProvider() {
+        return new Object[][] {
+                {"chrome"},
+                {"firefox"},
+                {"edge"}
+        };
+    }
+
+
+}
---
+++ java-automation/src/test/java/tests/BaseUITest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import Utils.SeleniumUtils;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class BaseUITest {
+
+    WebDriver driver;
+    String url;
+    String browser;
+    String signUpPath;
+    String signUpPageUrl;
+
+    @BeforeClass
+    public void setUp() throws IOException {
+        Properties properties = SeleniumUtils.readProperties("src\\test\\resources\\application.properties");
+        url = properties.getProperty("url");
+        signUpPath = properties.getProperty("signUpPath");
+        browser = properties.getProperty("browser");
+        signUpPageUrl = url + signUpPath;
+
+        System.out.println("Default browser: " + browser);
+        System.out.println("Page under test: " + signUpPageUrl);
+    }
+
+    @Test
+    public void Demo() {
+
+    }
+
+}
